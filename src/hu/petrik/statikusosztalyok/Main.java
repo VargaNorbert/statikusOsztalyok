97bc3adfa3f5261771c3a7205ca1a84cef68ae4c
Varga Norbert <vnorbi02@gmail.com>
2022-10-06 08:38:04 +0200

    alapok

--- src/hu/petrik/statikusosztalyok/Main.java
+++ src/hu/petrik/statikusosztalyok/Main.java
@@ -1,4 +1,11 @@
 package hu.petrik.statikusosztalyok;
 
 public class Main {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Veletlen.velEgesz(5,10));
+        }
+    }
 }
